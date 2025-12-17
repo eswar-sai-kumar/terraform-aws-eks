65de4cfd07e1a3cdc9abec179910113291b821ef
sai kumar lakkimsetti <eswarsaikumar2@gmail.com>
2025-12-17 18:19:29 +0530

    updated

--- 50-acm/parameters.tf
+++ 50-acm/parameters.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "acm_certificate_arn" {
   name  = "/${var.project_name}/${var.environment}/acm_certificate_arn"
   type  = "String"
-  value = aws_acm_certificate.expense.arn
+  # This forces SSM to wait until the DNS validation is actually finished
+  value = aws_acm_certificate_validation.expense.certificate_arn
 }
--- 50-acm/variables.tf
+++ 50-acm/variables.tf
@@ -1,5 +1,5 @@
 variable "project_name" {
-  default = ""
+  default = "expense"
 }
 
 variable "environment" {
